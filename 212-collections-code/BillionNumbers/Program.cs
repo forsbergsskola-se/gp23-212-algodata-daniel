b3bdb8d58a323a0eda853d5f003a1d3fbbef2770
sputgarin <sputgarin@protonmail.com>
2024-05-06 23:18:17 +0200

    Went through BillionNumbers and discussed with ChatGPT about it

---
+++ 212-collections-code/BillionNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+double sum = 0;
+foreach (var number in GetABillionNumbersList())
+{
+    sum += number;
+}
+
+Console.WriteLine(sum);
+
+// 20 bytes of RAM
+IEnumerable<int> GetABillionNumbers()
+{
+    for (int i = 0; i < 2_000_000_000; i++)
+    {
+        yield return i;
+    }
+}
+
+// at least 8 gigabytes of RAM
+IEnumerable<int> GetABillionNumbersList()
+{
+    List<int> numbers = new List<int>();
+    for (int i = 0; i < 2_000_000_000; i++)
+    {
+        numbers.Add(i);
+    }
+
+    return numbers;
+}
