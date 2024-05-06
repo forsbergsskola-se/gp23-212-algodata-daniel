f3a1b6758a3037e8afa0fef89d09df69ffa8cdf8
sputgarin <sputgarin@protonmail.com>
2024-05-06 22:30:11 +0200

    added the first code and discussed it with chatGPT

--- 212-collections-code/212-collections-code/Program.cs
+++ 212-collections-code/212-collections-code/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
 
-Console.WriteLine("Hello, World!");
+Vector3 position = new Vector3(12, 3, -4);
+
+foreach (var value in position)
+{
+    Console.WriteLine(value);
+}
+
+struct Vector3 : IEnumerable<float>
+{
+    public float x, y, z;
+
+    public Vector3(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public IEnumerator<float> GetEnumerator()
+    {
+        yield return x;
+        yield return y;
+        yield return z;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
