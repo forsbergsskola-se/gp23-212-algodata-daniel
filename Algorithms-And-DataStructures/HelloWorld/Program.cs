52cded2265b2455be6527cf01a5daa18dc6405be
sputgarin <sputgarin@protonmail.com>
2024-05-15 22:38:32 +0200

    Added the turbo collection for c#

---
+++ Algorithms-And-DataStructures/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TurboCollections;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TurboMaths.SayHello();
+        }
+    }
+}
