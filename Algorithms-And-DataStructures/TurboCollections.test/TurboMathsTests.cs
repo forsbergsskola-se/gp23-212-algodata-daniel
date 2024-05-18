d21a386dde0995631538ba90d2600ab6ff98b6cc
sputgarin <sputgarin@protonmail.com>
2024-05-18 02:05:58 +0200

    Added test to the c# environment

---
+++ Algorithms-And-DataStructures/TurboCollections.test/TurboMathsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using TurboCollections;
+
+namespace TurboCollections.test;
+
+public static class TurboMathsTests
+{
+    [Test]
+    public static void SayHelloExists()
+    {
+        TurboMaths.SayHello();
+        Assert.Pass();
+    }
+}
